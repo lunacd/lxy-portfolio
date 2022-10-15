8cec143c218e10d451a0c182290f7d6d9e5511a0
Haowen Liu <lhw@lunacd.com>
2022-10-14 18:46:05 -0700

    Add refugia manifesto
    Former-commit-id: 876355edaacb426940f924464c0e9c32256de1f4

--- pages/refugia.tsx
+++ pages/refugia.tsx
@@ -3,10 +3,13 @@ import React from "react";
 
 import ProjectNavigation from "../components/project-navigation";
 import Scroller from "../components/scroller";
+import TextImage from "../components/text-image";
 import TopDisplay from "../components/top-display";
 import { PageProps } from "../utils/page-props";
 import { projectData, projects } from "../utils/project-data";
 import useScroll from "../utils/use-scroll";
+
+import Manifesto from "../images/refugia/manifesto-half.webp";
 
 const Refugia: React.FC<PageProps> = (props) => {
   const scrollDiv = useScroll(!props.swipeAnimation);
@@ -20,6 +23,16 @@ const Refugia: React.FC<PageProps> = (props) => {
         <TopDisplay
           project={projectData[projects.indexOf("refugia")]}
           animation={false}
+        />
+
+        <TextImage
+          title="Project Manifesto"
+          content={[
+            "Refugia is an urban animal den for audiences to experience from animals' perspectives. It provides a sensory experience of animals' sight and feeling, and shows the adaptation of animals to the urban environment.  It's a good place for people to rethink their relationship with nature.",
+          ]}
+          image={Manifesto}
+          width={960}
+          height={620}
         />
 
         <div className="title text-center mb-32">
