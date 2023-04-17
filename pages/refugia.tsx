10b60d02ede17893c5776db6bc509866b4be55b3
Haowen Liu <lhw@lunacd.com>
2023-04-17 11:21:04 -0700

    Fix refugia error
    Former-commit-id: 75954ae2b386ba268a6b87beda2e3e3f8377d5b5 [formerly 3eb4892ec33baa4e9c4812d748058241eae91067]
    Former-commit-id: 62fa5277878bb9d1a4daa676a45f7597d8e46323
    Former-commit-id: 9f0f7ce6b8cf39ae3a075eb2be4a9232c681a11c

--- pages/refugia.tsx
+++ pages/refugia.tsx
@@ -8,10 +8,28 @@ import Spacing from "../components/spacing";
 import TextImage from "../components/text-image";
 import TopDisplay from "../components/top-display";
 import { PageProps } from "../utils/page-props";
-import { projectData, projects } from "../utils/project-data";
+import { ProjectData, projects } from "../utils/project-data";
 import useScroll from "../utils/use-scroll";
 
+import RefugiaSmall from "../images/projects/refugia-half.webp";
+import RefugiaDisplay from "../images/refugia/display-full.webp";
+import RefugiaDisplayM from "../images/refugia/display-mobile-half.webp";
 import Manifesto from "../images/refugia/manifesto-half.webp";
+
+const projectData = new ProjectData(
+  "refugia",
+  "Refugia",
+  "14 Weeks, 2022 Summer",
+  "Spatial Experience Design",
+  "Sustainability, Model making",
+  "See through their eyes; hear through their ears; sense through their senses. Learn and feel animals' adaptation to the urban environment.",
+  RefugiaDisplay,
+  RefugiaDisplayM,
+  RefugiaSmall,
+  "text-gray-100",
+  "bg-[#E0E4E0]",
+  "/refugia",
+);
 
 const Refugia: React.FC<PageProps> = (props) => {
   const scrollDiv = useScroll(!props.swipeAnimation);
@@ -23,7 +41,7 @@ const Refugia: React.FC<PageProps> = (props) => {
 
       <Scroller bgColor="bg-[#EEEAE2]" divRef={scrollDiv}>
         <TopDisplay
-          project={projectData[projects.indexOf("refugia")]}
+          project={projectData}
           animation={false}
           displayDescriptionOnMobile={true}
         />
