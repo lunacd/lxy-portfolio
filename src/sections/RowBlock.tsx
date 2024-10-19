81503bbbba7168b57e4458191c5981cd0473077e
Haowen Liu <lhw@lunacd.com>
2024-10-19 13:03:48 -0400

    Responsive row block

--- src/sections/RowBlock.tsx
+++ src/sections/RowBlock.tsx
@@ -1,5 +1,7 @@
+import styles from "./RowBlock.module.css";
 import { ProjectPage } from "@payload-types";
 import classNames from "classnames";
+import React from "react";
 
 import Block from "@/sections/Block";
 import { getSpacing } from "@/utils/spacingUtil";
@@ -9,11 +11,15 @@ type RowBlockProps = Extract<ProjectPage["blocks"][0], { blockType: "row" }>;
 export default function RowBlock(props: RowBlockProps) {
   return (
     <div
-      className={classNames("grid", { "mb-spacing-3lg": props.bottomMargin })}
-      style={{
-        gridTemplateColumns: `repeat(${props.blocks.length}, minmax(0, 1fr))`,
-        gap: `${getSpacing(props.spacing)}`,
-      }}
+      className={classNames(`grid ${styles.row}`, {
+        "mb-spacing-3lg": props.bottomMargin,
+      })}
+      style={
+        {
+          gap: `${getSpacing(props.spacing)}`,
+          "--column-count": props.blocks.length,
+        } as React.CSSProperties
+      }
     >
       {props.blocks.map((block, index) => (
         <Block key={index} block={block} />
