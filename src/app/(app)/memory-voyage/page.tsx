16778fc1881db8b1f1aa2d8928ad79dd21f37bcd
Haowen Liu <lhw@lunacd.com>
2024-12-30 17:48:10 -0500

    Add memory voyage

--- src/app/(app)/memory-voyage/page.tsx
+++ src/app/(app)/memory-voyage/page.tsx
@@ -10,26 +10,26 @@ import Scroller from "@/components/Scroller";
 import TopDisplay from "@/components/TopDisplay";
 
 export const metadata: Metadata = {
-  title: "M-Tron | Shirley Lyu",
+  title: "Memory Voyage | Shirley Lyu",
 };
 
-export default async function MTron() {
+export default async function MemoryVoyage() {
   const payload = await getPayload({
     config,
   });
-  const mTronData = (
+  const memoryVoyageData = (
     await payload.find({
       collection: "projects",
       where: {
-        uri: { equals: "m-tron" },
+        uri: { equals: "memory-voyage" },
       },
     })
   ).docs[0];
-  const mTronPageData = (
+  const memoryVoyagePageData = (
     await payload.find({
       collection: "projectPages",
       where: {
-        projectName: { equals: "M-Tron" },
+        projectName: { equals: "Memory Voyage" },
       },
     })
   ).docs[0];
@@ -37,17 +37,19 @@ export default async function MTron() {
   return (
     <AnimationCoordinator>
       <Scroller
-        bgColor={mTronData.pageBackgroundColor ?? mTronData.backgroundColor}
+        bgColor={
+          memoryVoyageData.pageBackgroundColor ??
+          memoryVoyageData.backgroundColor
+        }
       >
-        <TopDisplay project={mTronData} payload={payload} />
-        {mTronPageData.blocks.map((block, index) => (
+        <TopDisplay project={memoryVoyageData} payload={payload} />
+        {memoryVoyagePageData.blocks.map((block, index) => (
           <FloatUpMotion className="single" key={index}>
             <Block block={block} payload={payload} />
           </FloatUpMotion>
         ))}
         <ProjectNavigation
-          prev="/sunrise"
-          next="/memory-voyage"
+          prev="/m-tron"
           textColor="text-white"
           payload={payload}
         />
