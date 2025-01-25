6499e1d8452d24537fddb1f7332fa58d0bfd0450
Haowen Liu <lhw@lunacd.com>
2025-01-25 10:13:30 -0500

    Overlap take blocks from payload

--- src/app/(app)/overlap/page.tsx
+++ src/app/(app)/overlap/page.tsx
@@ -3,6 +3,7 @@ import config from "@payload-config";
 import { Metadata } from "next";
 import { getPayload } from "payload";
 
+import Blocks from "@/blocks/Blocks";
 import ProjectNavigation from "@/components/ProjectNavigation";
 import Scroller from "@/components/Scroller";
 import TopDisplay from "@/components/TopDisplay";
@@ -23,10 +24,23 @@ export default async function Overlap() {
       },
     })
   ).docs[0];
+  const overlapPageData = (
+    await payload.find({
+      collection: "projectPages",
+      where: {
+        projectName: { equals: "Overlap" },
+      },
+    })
+  ).docs[0];
   return (
     <Scroller bgColor={overlapData.backgroundColor}>
       <OverlapClient
-        topChildren={<TopDisplay project={overlapData} payload={payload} />}
+        topChildren={
+          <>
+            <TopDisplay project={overlapData} payload={payload} />
+            <Blocks blocks={overlapPageData.blocks} payload={payload} />
+          </>
+        }
       >
         <ProjectNavigation
           prev="/soul"
