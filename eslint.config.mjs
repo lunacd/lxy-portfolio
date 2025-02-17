e2b681eabd6ecd733430adbd7001a58e1f45eafa
Haowen Liu <lhw@lunacd.com>
2025-02-17 17:20:09 -0500

    Move to nextjs recommended config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,13 @@
-import nextPlugin from "@next/eslint-plugin-next";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    plugins: {
-      "@next/next": nextPlugin,
-      "@typescript-eslint": tsPlugin,
-    },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: { modules: true },
-        ecmaVersion: "latest",
-        project: "./tsconfig.json",
-      },
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
-      ...tsPlugin.configs["eslint-recommended"].rules,
-      ...tsPlugin.configs["recommended"].rules,
-    },
-  },
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+  }),
 ];
+
+export default eslintConfig;
