8136453e1567844875d3f37047c88133d82db6ab
Haowen Liu <lhw@lunacd.com>
2025-03-15 21:55:41 -0400

    Move lyu to payload

--- src/app/(app)/lyu/page.tsx
+++ src/app/(app)/lyu/page.tsx
@@ -1,9 +1,10 @@
-import LyuClient from "./page.client";
 import config from "@payload-config";
 import { Metadata } from "next";
 import { getPayload } from "payload";
 
+import Block from "@/blocks/Block";
 import ConnectPrompt from "@/components/ConnectPrompt";
+import FloatUpMotion from "@/components/FloatUpMotion";
 import RelatedWork from "@/components/RelatedWork";
 import Scroller from "@/components/Scroller";
 import TopDisplay from "@/components/TopDisplay";
@@ -25,20 +26,30 @@ export default async function Lyu() {
       depth: 2,
     })
   ).docs[0];
+  const lyuPageData = (
+    await payload.find({
+      collection: "projectPages",
+      where: {
+        projectName: { equals: "Lyu" },
+      },
+    })
+  ).docs[0];
   return (
     <Scroller bgColor={lyuData.backgroundColor}>
-      <LyuClient
-        topChildren={<TopDisplay project={lyuData} payload={payload} />}
-      >
-        <RelatedWork
-          color={lyuData.contentColor}
-          projects={lyuData.relatedWorks.map(
-            (relatedWork) => relatedWork.relatedWork,
-          )}
-          payload={payload}
-        />
-        <ConnectPrompt payload={payload} />
-      </LyuClient>
+      <TopDisplay project={lyuData} payload={payload} />
+      {lyuPageData.blocks.map((block, index) => (
+        <FloatUpMotion className="single" key={index}>
+          <Block block={block} payload={payload} />
+        </FloatUpMotion>
+      ))}
+      <RelatedWork
+        color={lyuData.contentColor}
+        projects={lyuData.relatedWorks.map(
+          (relatedWork) => relatedWork.relatedWork,
+        )}
+        payload={payload}
+      />
+      <ConnectPrompt payload={payload} />
     </Scroller>
   );
 }
