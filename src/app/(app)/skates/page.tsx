4acb532167d5e6560ddcf40b514904e2f22e0fc5
Haowen Liu <lhw@lunacd.com>
2025-03-16 12:54:38 -0400

    Add payload content to skates

--- src/app/(app)/skates/page.tsx
+++ src/app/(app)/skates/page.tsx
@@ -3,6 +3,8 @@ import config from "@payload-config";
 import { Metadata } from "next";
 import { getPayload } from "payload";
 
+import Blocks from "@/blocks/Blocks";
+import ConnectPrompt from "@/components/ConnectPrompt";
 import RelatedWork from "@/components/RelatedWork";
 import Scroller from "@/components/Scroller";
 import TopDisplay from "@/components/TopDisplay";
@@ -24,18 +26,33 @@ export default async function Skates() {
       depth: 2,
     })
   ).docs[0];
+  const skatesPageData = (
+    await payload.find({
+      collection: "projectPages",
+      where: {
+        projectName: { equals: "Skates" },
+      },
+    })
+  ).docs[0];
   return (
     <Scroller bgColor={skatesData.backgroundColor}>
       <SkatesClient
-        topChildren={<TopDisplay project={skatesData} payload={payload} />}
-      ></SkatesClient>
-      <RelatedWork
-        color={skatesData.contentColor}
-        projects={skatesData.relatedWorks.map(
-          (relatedWork) => relatedWork.relatedWork,
-        )}
-        payload={payload}
-      />
+        topChildren={
+          <>
+            <TopDisplay project={skatesData} payload={payload} />
+            <Blocks blocks={skatesPageData.blocks} payload={payload} />
+          </>
+        }
+      >
+        <RelatedWork
+          color={skatesData.contentColor}
+          projects={skatesData.relatedWorks.map(
+            (relatedWork) => relatedWork.relatedWork,
+          )}
+          payload={payload}
+        />
+        <ConnectPrompt payload={payload} />
+      </SkatesClient>
     </Scroller>
   );
 }
