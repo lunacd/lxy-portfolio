fe8a0784bf01498fbe6a582488cf6d2c6e604b65
Haowen Liu <lhw@lunacd.com>
2025-11-30 19:52:42 -0500

    Fix sizing for under-sized images

--- src/components/PayloadImageInteractive.tsx
+++ src/components/PayloadImageInteractive.tsx
@@ -60,6 +60,7 @@ export default function PayloadImageInteractive(
           className={classNames({
             [`absolute top-1/2 left-1/2 max-h-full max-w-full -translate-x-1/2
             -translate-y-1/2`]: isZoomed,
+            "w-full": !isZoomed
           })}
           style={{
             flex: props.equalHeight
@@ -69,7 +70,6 @@ export default function PayloadImageInteractive(
           }}
           fill={props.fill}
           sizes={props.sizes}
-          // unoptimized={props.unoptimized}
           unoptimized
           objectFit={isZoomed ? "contain" : props.objectFit}
           objectPosition={isZoomed ? undefined : props.objectPosition}
