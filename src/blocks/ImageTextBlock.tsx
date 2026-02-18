383f7cda1be80dbecd27ab966ad00db5e510a7eb
Haowen Liu <lhw@lunacd.com>
2026-02-17 20:49:36 -0500

    Support mobile viewing in ImageText

--- src/blocks/ImageTextBlock.tsx
+++ src/blocks/ImageTextBlock.tsx
@@ -1,9 +1,11 @@
 import TextBlock from "./TextBlock";
 import { ProjectPage } from "@payload-types";
+import classNames from "classnames";
 import { Payload } from "payload";
 import "server-only";
 
 import SlideShow from "@/components/SlideShow";
+import Text from "@/components/Text";
 import { unwrapImages } from "@/utils/payloadHelpers";
 import { getSpacing } from "@/utils/spacingUtil";
 
@@ -15,7 +17,7 @@ type ImageTextBlockProps = Extract<
 export default function ImageTextBlock(props: ImageTextBlockProps) {
   return (
     <div
-      className="gap-spacing grid grid-cols-3 items-end"
+      className="gap-spacing row-start-2 grid grid-cols-3 items-end md:row-auto"
       style={{
         marginBottom: getSpacing(props.bottomMargin),
       }}
@@ -23,15 +25,20 @@ export default function ImageTextBlock(props: ImageTextBlockProps) {
       <SlideShow
         images={props.images.map(unwrapImages)}
         payload={props.payload}
-        className="col-span-2"
+        className="col-span-3 md:col-span-2"
         sizes="67vw"
       />
-      <TextBlock
-        blockType="text"
+      <Text
+        className={classNames(
+          "col-span-3 row-start-1 md:col-span-1 md:row-auto",
+          {
+            "text-white": props.textColor === "light",
+            "text-black": props.textColor === "dark",
+          },
+        )}
+        style={{ marginBottom: getSpacing(props.bottomMargin) }}
         text={props.text}
-        textColor={props.textColor}
-        bottomMargin="none"
-      ></TextBlock>
+      ></Text>
     </div>
   );
 }
